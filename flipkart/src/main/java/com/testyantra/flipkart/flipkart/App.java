e72ce98a99f4b458aa8ce6fa8c6fa9b1efcbcc62
TYSS <TYSS@TYSS-TR-006>
2018-06-18 16:37:11 +0530

    1st Commit

---
+++ flipkart/src/main/java/com/testyantra/flipkart/flipkart/App.java
@@ -0,0 +1,16 @@
+package com.testyantra.flipkart.flipkart;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	@Test
+	public void flip()
+    {
+        System.out.println( "Hello World!" );
+    }
+}
